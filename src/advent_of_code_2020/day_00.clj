c205cfc0acc2367dcf9d39f4e9475be4a774f35e
Oscar Marshall <omarshal@meraki.com>
2020-12-06 00:27:40 -0800

    Set up a day template

---
+++ src/advent_of_code_2020/day_00.clj
@@ -0,0 +1,35 @@
+(ns advent-of-code-2020.day-00
+  (:require [advent-of-code-2020.core :as core]))
+
+;;; Part 1
+;;; ============================================================================
+
+(def input (core/get-input))
+
+(defn parse-input [input]
+  input)
+
+(def parsed-input (parse-input input))
+
+(defn answer-part-1 [parsed-input]
+  parsed-input)
+
+(def part-1-answer (answer-part-1 parsed-input))
+
+(comment
+  part-1-answer
+
+  )
+
+;;; Part 2
+;;; ============================================================================
+
+(defn answer-part-2 [parsed-input]
+  parsed-input)
+
+(def part-2-answer (answer-part-2 parsed-input))
+
+(comment
+  part-2-answer
+
+  )
---
+++ src/user.clj
@@ -0,0 +1,8 @@
+(ns user
+  (:require [clojure.string :as string]))
+
+(defn init-day [day]
+  (-> "src/advent_of_code_2020/day_00.clj"
+      slurp
+      (string/replace "00" (format "%02d" day))
+      (->> (spit (format "src/advent_of_code_2020/day_%02d.clj" day)))))
