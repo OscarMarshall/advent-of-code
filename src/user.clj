bc22f3bd224550f172f80fb1784d620089618d12
Oscar Marshall <omarshal@meraki.com>
2020-12-06 22:48:23 -0800

    Add input file generation

--- src/user.clj
+++ src/user.clj
@@ -5,4 +5,5 @@
   (-> "src/advent_of_code_2020/day_00.clj"
       slurp
       (string/replace "00" (format "%02d" day))
-      (->> (spit (format "src/advent_of_code_2020/day_%02d.clj" day)))))
+      (->> (spit (format "src/advent_of_code_2020/day_%02d.clj" day))))
+  (spit (format "src/advent_of_code_2020/day_%02d_input.txt" day) ""))
