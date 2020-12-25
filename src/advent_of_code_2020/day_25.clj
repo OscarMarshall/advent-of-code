fce2710daa0239dc7a2402261087865b6a420737
Oscar Marshall <omarshal@meraki.com>
2020-12-25 03:07:48 -0800

    Day 25

---
+++ src/advent_of_code_2020/day_25.clj
@@ -0,0 +1,31 @@
+(ns advent-of-code-2020.day-25
+  (:require [advent-of-code-2020.core :as core]
+            [clojure.string :as string]))
+
+;;; Part 1
+;;; ============================================================================
+
+(def input (core/get-input))
+
+(defn parse-input [input]
+  (map #(Long/parseLong %) (string/split-lines input)))
+
+(def parsed-input (parse-input input))
+
+(defn step [x subject-number]
+  (rem (* x subject-number) 20201227))
+
+(defn answer-part-1 [parsed-input]
+  (let [[card-public-key door-public-key] parsed-input
+
+        card-loops
+        (count (take-while (complement #{card-public-key})
+                           (iterate #(step % 7) 1)))]
+    (nth (iterate #(step % door-public-key) 1) card-loops)))
+
+(def part-1-answer (answer-part-1 parsed-input))
+
+(comment
+  part-1-answer
+  ;; => 15467093
+  )
