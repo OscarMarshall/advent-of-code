40eba3ae54470055e4a9ac3a4f0bdd248d390864
Oscar Marshall <omarshal@meraki.com>
2021-12-05 03:00:22 -0800

    [2021] Day 05

---
+++ src/advent_of_code/year_2021/day_05.clj
@@ -0,0 +1,58 @@
+(ns advent-of-code.year-2021.day-05
+  (:require [advent-of-code.core :as core]
+            [clojure.string :as string]))
+
+(def input (core/get-input))
+
+(defn parse-input [input]
+  (map (fn [line]
+         (->> line
+              (re-matches #"(\d+),(\d+) -> (\d+),(\d+)")
+              rest
+              (map #(Long/parseLong %))
+              (partition 2)
+              vec))
+       (string/split-lines input)))
+
+(def parsed-input (parse-input input))
+
+
+;;; Part 1
+;;; ============================================================================
+
+(defn line->points [[[x1 y1] [x2 y2]]]
+  (into #{[x2 y2]}
+        (cond
+          (= x1 x2) (map (fn [y] [x1 y])
+                         (range y1 y2 (if (pos? (- y2 y1)) 1 -1)))
+          (= y1 y2) (map (fn [x] [x y1])
+                         (range x1 x2 (if (pos? (- x2 x1)) 1 -1)))
+          :else     (map vector
+                         (range x1 x2 (if (pos? (- x2 x1)) 1 -1))
+                         (range y1 y2 (if (pos? (- y2 y1)) 1 -1))))))
+
+(defn number-of-overlapping-points [lines]
+  (->> lines
+       (mapcat line->points)
+       frequencies
+       (filter (comp (partial < 1) val))
+       count))
+
+(defn answer-part-1 [parsed-input]
+  (->> parsed-input
+       (filter (comp (partial some true?) (partial apply map =)))
+       number-of-overlapping-points))
+
+(def part-1-answer (answer-part-1 parsed-input))
+
+(assert (= part-1-answer 4728))
+
+
+;;; Part 2
+;;; ============================================================================
+
+(defn answer-part-2 [parsed-input] (number-of-overlapping-points parsed-input))
+
+(def part-2-answer (answer-part-2 parsed-input))
+
+(assert (= part-2-answer 17717))
