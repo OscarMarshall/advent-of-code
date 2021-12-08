ef4b9c53ff3492093aee75dc338a36d1ae9f50e8
Oscar Marshall <omarshal@meraki.com>
2021-12-08 10:48:02 -0800

    [2021] Simplify day 08

--- src/advent_of_code/year_2021/day_08.clj
+++ src/advent_of_code/year_2021/day_08.clj
@@ -1,8 +1,7 @@
 (ns advent-of-code.year-2021.day-08
   (:require [advent-of-code.core :as core]
             [clojure.string :as string]
-            [clojure.set :as set])
-  (:import (clojure.lang PersistentQueue)))
+            [clojure.set :as set]))
 
 (def input (core/get-input))
 
@@ -35,86 +34,47 @@
 ;;; Part 2
 ;;; ============================================================================
 
-(def segments->number
-  {#{\a \b \c \e \f \g}    0
-   #{\c \f}                1
-   #{\a \c \d \e \g}       2
-   #{\a \c \d \f \g}       3
-   #{\b \c \d \f}          4
-   #{\a \b \d \f \g}       5
-   #{\a \b \d \e \f \g}    6
-   #{\a \c \f}             7
-   #{\a \b \c \d \e \f \g} 8
-   #{\a \b \c \d \f \g}    9})
+(def number->segments
+  {0 #{\a \b \c \e \f \g}
+   1 #{\c \f}
+   2 #{\a \c \d \e \g}
+   3 #{\a \c \d \f \g}
+   4 #{\b \c \d \f}
+   5 #{\a \b \d \f \g}
+   6 #{\a \b \d \e \f \g}
+   7 #{\a \c \f}
+   8 #{\a \b \c \d \e \f \g}
+   9 #{\a \b \c \d \f \g}})
 
-(def all-segments (reduce into (keys segments->number)))
+(def four-segments (number->segments 4))
 
-(def number->segments (set/map-invert segments->number))
+(def segments->number (set/map-invert number->segments))
 
-(def number-of-segments->possible-numbers
-  (into {}
-        (map (juxt key (comp (partial map val) val)))
-        (group-by (comp count key) segments->number)))
-
-(def segment->number-of-occurences
-  (frequencies (apply concat (keys segments->number))))
-
-(def number-of-occurences->possible-segments
+(def frequency->segments
   (reduce (fn [acc [k v]] (update acc v (fnil conj #{}) k))
           {}
-          segment->number-of-occurences))
-
-(defn which-number [wire->possible-segments wires]
-  (let [possible-numbers (number-of-segments->possible-numbers (count wires))]
-    (if (= (count possible-numbers) 1)
-      (first possible-numbers)
-      (let [possible-numbers
-            (filter (fn [possible-number]
-                      (let [number-segments (number->segments possible-number)]
-                        (every? (comp seq
-                                      (partial set/intersection number-segments)
-                                      wire->possible-segments)
-                                wires)))
-                    possible-numbers)]
-        (when (= (count possible-numbers) 1)
-          (first possible-numbers))))))
+          (frequencies (into () (mapcat val) number->segments))))
 
 (defn decode-display [{:keys [examples output]}]
-  (let [wire->number-of-occurences (frequencies (apply concat examples))]
-    (loop [wire->possible-segments
-           (into {} (map (juxt identity
-                               (comp number-of-occurences->possible-segments
-                                     wire->number-of-occurences))
-                         all-segments))
-
-           examples
-           (into PersistentQueue/EMPTY
-                 (sort-by (comp count
-                                number-of-segments->possible-numbers
-                                count)
-                          examples))]
-      (let [example  (peek examples)
-            examples (pop examples)]
-        (if (nil? example)
-          (let [wire->segments (into {}
-                                     (map (juxt key (comp first val)))
-                                     wire->possible-segments)]
-            (transduce (map (comp segments->number
-                                  set
-                                  (partial map wire->segments)))
-                       (completing (fn [acc x] (+ (* acc 10) x)))
-                       0
-                       output))
-          (if-let [number (which-number wire->possible-segments example)]
-            (let [number-segments (number->segments number)]
-              (recur (reduce (fn [wire->possible-segments wire]
-                               (update wire->possible-segments
-                                       wire
-                                       set/intersection
-                                       number-segments))
-                             wire->possible-segments example)
-                     examples))
-            (recur wire->possible-segments (conj examples example))))))))
+  (let [four-wires     (some (fn [example] (when (= (count example) 4) example))
+                             examples)
+        wire->segments (into {}
+                             (map (fn [[wire frequency]]
+                                    (let [segments (frequency->segments
+                                                    frequency)]
+                                      [wire (first (cond-> segments
+                                                     (> (count segments) 1)
+                                                     ((if (four-wires wire)
+                                                        set/intersection
+                                                        set/difference)
+                                                      four-segments)))])))
+                             (frequencies (apply concat examples)))]
+    (transduce (map (comp segments->number
+                          set
+                          (partial map wire->segments)))
+               (completing (fn [acc x] (+ (* acc 10) x)))
+               0
+               output)))
 
 (defn answer-part-2 [parsed-input]
   (transduce (map decode-display) + parsed-input))
