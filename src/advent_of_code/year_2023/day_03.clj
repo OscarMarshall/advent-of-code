d6a885010f5909500e48783700fe39aa396ec2fd
Oscar Marshall <oscar.lim.marshall@gmail.com>
2023-12-02 22:20:35 -0800

    [2023] Day 3: Part 1 Submission 3 (correct)
    Had a bug where numbers on the edge were always included. I define
    symbol to be not a digit or \., therefore nil, which coordinates outside
    the grid will return, will be considered as symbols.

--- src/advent_of_code/year_2023/day_03.clj
+++ src/advent_of_code/year_2023/day_03.clj
@@ -24,22 +24,23 @@
 
 (defn number-neighbor-coordinates [[row column :as coordinates] schematic]
   (let [column-start (dec column)
-        column-end   (+ column (number-length coordinates schematic))]
-    (concat (for [row2    [(dec row) (inc row)]
-                  column2 (range column-start (inc column-end))]
-              [row2 column2])
-            (map (fn [column2] [row column2])
-                 (range column-start (inc column-end))))))
+        column-end   (+ column (number-length coordinates schematic))
+        column-range (range column-start (inc column-end))]
+    (conj (for [row2 [(dec row) (inc row)], column2 column-range]
+            [row2 column2])
+          [row column-start]
+          [row column-end])))
 
 (defn part-number-coordinate? [coordinates schematic]
   (some (fn [coordinates2]
-          (not ((conj digits \.) (get-in schematic coordinates2))))
+          (not ((conj digits \.) (get-in schematic coordinates2 \.))))
         (number-neighbor-coordinates coordinates schematic)))
 
 (defn all-coordinates [schematic]
-  (for [row    (range (schematic-height schematic))
-        column (range (schematic-width schematic))]
-    [row column]))
+  (let [width (schematic-width schematic)]
+    (for [row    (range (schematic-height schematic))
+          column (range width)]
+      [row column])))
 
 (defn number-coordinates [schematic]
   (filter (fn [[row column :as coordinates]]
@@ -52,7 +53,7 @@
                      (subvec (schematic row)
                              column
                              (+ column
-                                (part-number-length coordinate schematic))))))
+                                (number-length coordinate schematic))))))
 
 (defn answer-part-1 [schematic]
   (apply + (map #(coordinate->part-number % schematic)
@@ -62,7 +63,8 @@
 (def part-1-answer (answer-part-1 parsed-input))
 
 (assert (not= part-1-answer 291177))
-(assert (= part-1-answer 534693))
+(assert (not= part-1-answer 534693))
+(assert (= part-1-answer 528819))
 
 
 ;;; Part 2
