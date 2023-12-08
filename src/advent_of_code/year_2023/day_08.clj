845f1f3d188ef46d298d4bf961879679df086e4f
Oscar Marshall <oscar.lim.marshall@gmail.com>
2023-12-08 13:56:29 -0800

    [2023] Day 8: Part 2 Submission 1 (Correct)

--- src/advent_of_code/year_2023/day_08.clj
+++ src/advent_of_code/year_2023/day_08.clj
@@ -1,7 +1,8 @@
 (ns advent-of-code.year-2023.day-08
   (:require [advent-of-code.core :as core]
             [clojure.string :as str]
-            [medley.core :as medley]))
+            [medley.core :as medley]
+            [clojure.math :as math]))
 
 (println "# Day " 8)
 
@@ -30,13 +31,15 @@
 
 (println "## Part 1")
 
-(defn answer-part-1 [{:keys [instructions nodes]}]
+(defn answer [{:keys [instructions nodes]} start ends]
   (->> (reductions (fn [id instruction]
                      ((case instruction \L :left, \R :right) (nodes id)))
-                   "AAA"
+                   start
                    (cycle instructions))
-       (take-while (complement #{"ZZZ"}))
+       (take-while (complement ends))
        count))
+
+(defn answer-part-1 [network] (answer network "AAA" #{"ZZZ"}))
 
 (println "### Sample Answer")
 
@@ -60,14 +63,51 @@
 
 (println "## Part 2")
 
-(defn answer-part-2 [x]
-  x)
+(defn starting-ids [ids]
+  (filter #(re-matches #"..A" %) ids))
+
+(defn ending-ids [ids]
+  (set (filter #(re-matches #"..Z" %) ids)))
+
+(defn factors
+  ([n] (factors n 2))
+  ([n last-checked]
+   (if-some [factor (first (filter #(zero? (rem n %))
+                                   (range last-checked (math/sqrt n))))]
+     (cons factor (factors (/ n factor) factor))
+     [n])))
+
+(defn answer-part-2 [{:as network, :keys [nodes]}]
+  (let [ending-ids (ending-ids (keys nodes))]
+    (->> nodes
+         keys
+         starting-ids
+         (map #(frequencies (factors (answer network % ending-ids))))
+         (apply merge-with max)
+         (mapcat (fn [[x n]] (repeat n x)))
+         (apply *))))
+
+(comment
+  (starting-ids (keys (:nodes parsed-input)))
+  )
 
 (println "### Sample Answer")
 
-(def part-2-sample-answer (time (answer-part-2 sample-parsed-input)))
+(def sample2
+  "LR
 
-(prn part-2-sample-answer)
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)")
+
+(def part-2-sample-answer (time (answer-part-2 (parse-input sample2))))
+
+(prn part-2-sample-answer 6)
 (println)
 
 (println "### Answer")
@@ -78,4 +118,4 @@
 (println)
 
 (assert (= part-2-sample-answer))
-(assert (= part-2-answer part-2-answer))
+(assert (= part-2-answer 21165830176709))
