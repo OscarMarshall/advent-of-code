b0b779a21a5058d74868b7665ac6e27391cd3aef
Oscar Marshall <oscar.lim.marshall@gmail.com>
2023-12-14 21:15:37 -0800

    15.1

---
+++ src/advent_of_code/year_2023/day_15.clj
@@ -0,0 +1,44 @@
+(ns advent-of-code.year-2023.day-15
+  (:require [advent-of-code.core :as core]
+            [clojure.string :as str]))
+
+(println "# Day 15")
+
+(set! *warn-on-reflection* true)
+
+(defn parse-input [input] (str/split input #","))
+
+(comment
+  (core/current-parsed-input :sample1)
+  (core/current-parsed-input)
+  )
+
+;;;; Part 1
+
+(defn hash-instruction [s]
+  (transduce (map long)
+             (completing (fn [current-value ascii-code]
+                           (mod (* (+ current-value ascii-code) 17) 256)))
+             0
+             s))
+
+(defn answer-part-1 [instructions]
+  (transduce (map hash-instruction)
+             +
+             instructions))
+
+(core/part 1
+  parse-input answer-part-1 *file*
+  [:sample1 52]
+  [:input 517847])
+
+
+;;;; Part 2
+
+(defn answer-part-2 [x]
+  x)
+
+(core/part 2
+  parse-input answer-part-2 *file*
+  [:sample1 #_?]
+  [:input #_(core/current-answer 2)])
