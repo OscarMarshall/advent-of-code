2526805952cfb5e03e46456a3258e84afe991470
Oscar Marshall <oscar.lim.marshall@gmail.com>
2023-12-14 21:56:24 -0800

    15.2

--- src/advent_of_code/year_2023/day_15.clj
+++ src/advent_of_code/year_2023/day_15.clj
@@ -1,6 +1,8 @@
 (ns advent-of-code.year-2023.day-15
   (:require [advent-of-code.core :as core]
-            [clojure.string :as str]))
+            [clojure.string :as str]
+            [medley.core :as medley])
+  (:import (java.util List)))
 
 (println "# Day 15")
 
@@ -15,7 +17,7 @@
 
 ;;;; Part 1
 
-(defn hash-instruction [s]
+(defn hash-algorithm [s]
   (transduce (map long)
              (completing (fn [current-value ascii-code]
                            (mod (* (+ current-value ascii-code) 17) 256)))
@@ -23,7 +25,7 @@
              s))
 
 (defn answer-part-1 [instructions]
-  (transduce (map hash-instruction)
+  (transduce (map hash-algorithm)
              +
              instructions))
 
@@ -35,10 +37,40 @@
 
 ;;;; Part 2
 
-(defn answer-part-2 [x]
-  x)
+(defn perform-step [boxes s]
+  (condp re-matches s
+    #"(.*)-"      :>> (fn [[_ label]]
+                        (update
+                         boxes
+                         (hash-algorithm label)
+                         (fnil (fn [[^List order focal-lengths :as box]]
+                                 (let [index (.indexOf order label)]
+                                   (if (not= index -1)
+                                     [(vec (medley/remove-nth index order))
+                                      focal-lengths]
+                                     box)))
+                               [[] {}])))
+    #"(.*)=(\d+)" :>> (fn [[_ label focal-length]]
+                        (update boxes
+                                (hash-algorithm label)
+                                (fnil (fn [[^List order focal-lengths]]
+                                        (let [index (.indexOf order label)]
+                                          [(cond-> order
+                                             (= index -1) (conj label))
+                                           (assoc focal-lengths
+                                                  label
+                                                  (parse-long focal-length))]))
+                                      [[] {}])))))
+
+(defn calculate-total-focusing-power [boxes]
+  (apply + (for [[box [order focal-lengths]] boxes
+                 [index label]               (map-indexed vector order)]
+             (* (inc box) (inc index) (focal-lengths label)))))
+
+(defn answer-part-2 [instructions]
+  (calculate-total-focusing-power (reduce perform-step {} instructions)))
 
 (core/part 2
   parse-input answer-part-2 *file*
-  [:sample1 #_?]
-  [:input #_(core/current-answer 2)])
+  [:sample1 145]
+  [:input 267372])
